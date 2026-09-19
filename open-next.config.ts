3b894a388c92c70d6c50ae6cd8b05fe5c32f76ca
Shubham-hahh <swaruproy192@gmail.com>
2026-09-19 21:11:42 +0530

    Update deployment environment to Ubuntu 24.04 and change incremental cache to static assets

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,6 +1,6 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
-import kvIncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/kv-incremental-cache";
+import staticAssetsCache from "@opennextjs/cloudflare/overrides/incremental-cache/static-assets-incremental-cache";
 
 export default defineCloudflareConfig({
-	incrementalCache: kvIncrementalCache,
+	incrementalCache: staticAssetsCache,
 });
